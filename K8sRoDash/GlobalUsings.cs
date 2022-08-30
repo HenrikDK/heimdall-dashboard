9722c3dfef47d4d7eab6ed8c0f2a3ae01994815a
HenrikDK <commander.john.crichton@gmail.com>
2022-08-31 01:37:00 +0200

    cleanup globalusings

--- K8sRoDash/GlobalUsings.cs
+++ K8sRoDash/GlobalUsings.cs
@@ -1,27 +1,16 @@
 global using System;
 global using System.Diagnostics;
-global using System.Linq;
-global using System.Net;
 global using System.Collections.Generic;
-global using System.ComponentModel.DataAnnotations;
-global using System.Text;
-global using System.Text.RegularExpressions;
-global using System.Transactions;
 global using System.Dynamic;
 global using System.Runtime.InteropServices;
 
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
-global using Microsoft.AspNetCore.Html;
-global using Microsoft.AspNetCore.Http.Extensions;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using Microsoft.Extensions.Caching.Memory;
-global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
 global using AspNetCore.Proxy;
-global using Flurl;
 global using Lamar;
 global using Lamar.Microsoft.DependencyInjection;
 global using Newtonsoft.Json;
