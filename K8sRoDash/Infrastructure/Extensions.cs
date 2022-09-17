93e00d90d123efc1eb58b905cd437c824d8bf14a
HenrikDK <commander.john.crichton@gmail.com>
2022-09-17 21:00:33 +0200

    add extensions

---
+++ K8sRoDash/Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+namespace K8sRoDash.Infrastructure;
+
+public static class Extensions
+{
+    public static string ToBase64(this string value)
+    {
+        if (string.IsNullOrEmpty(value)) return value;
+        
+        var bytes = Encoding.UTF8.GetBytes(value);
+        return Convert.ToBase64String(bytes);
+    }
+
+    public static string FromBase64(string value) {
+        if (string.IsNullOrEmpty(value)) return value;
+
+        var bytes = Convert.FromBase64String(value);
+        return Encoding.UTF8.GetString(bytes);
+    }
+}
