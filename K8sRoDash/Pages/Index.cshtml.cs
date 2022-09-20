407f16a78dfdd7ce98a1c57a482157244441cacf
HenrikDK <commander.john.crichton@gmail.com>
2022-09-20 02:00:07 +0200

    add endpoint to fetch configured filters

--- K8sRoDash/Pages/Index.cshtml.cs
+++ K8sRoDash/Pages/Index.cshtml.cs
@@ -2,4 +2,18 @@
 
 public class IndexModel : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public IndexModel(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public JsonResult OnGetFilters()
+    {
+        var filters = _configuration.GetValue("filters", "").Split(",")
+            .Select(x => x.Trim()).ToList();
+
+        return new JsonResult(filters);
+    }
 }
