6447626fbc69539a0082448f6e33e013367f3c6c
HenrikDK <commander.john.crichton@gmail.com>
2022-09-23 01:33:46 +0200

    helper function to filter against list

--- K8sRoDash/wwwroot/js/site.js
+++ K8sRoDash/wwwroot/js/site.js
@@ -1,3 +1,11 @@
 ﻿var mixinArray = [];
 var components = {};
 var routes = []
+
+let nsEndsWith = (ns, ends) => {
+    let value = false;
+    value = ends.some(element => {
+        return ns.endsWith(element);
+    });
+    return value;
+};
