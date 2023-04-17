050cc17131f934d2a9b6e16fdcd8e71620bb9dbf
HenrikDK <commander.john.crichton@gmail.com>
2023-04-17 03:53:34 +0200

    prometheus queries in line with what lens expects

--- Heimdall.Dashboard.Ui/wwwroot/js/site.js
+++ Heimdall.Dashboard.Ui/wwwroot/js/site.js
@@ -83,16 +83,16 @@ function getMetricQuery(type = '', name = '', options = {}) {
         case "cluster":
             switch (name) {
                 case "node-memory-stats":
-                    return `sum({__name__=~"node_memory_MemTotal_bytes|node_memory_MemFree_bytes|node_memory_Buffers_bytes|node_memory_Cached_bytes", node=~"${options.nodes}"}) by (__name__, component)`;
+                    return `sum({__name__=~"node_memory_MemTotal_bytes|node_memory_MemFree_bytes|node_memory_Buffers_bytes|node_memory_Cached_bytes", kubernetes_node=~"${options.nodes}"}) by (__name__, component)`;
                 case "container-memory":
                     return `sum(container_memory_working_set_bytes{container!="POD",container!="",instance=~"${options.nodes}"}) by (component)`;
                 case "kube-memory-stats":
-                    return `sum({__name__=~"kube_pod_container_resource_requests|kube_pod_container_resource_limits|kube_node_status_capacity|kube_node_status_allocatable", node=~"${options.nodes}", resource="memory"}) by (__name__, component)`;
+                    return `sum({__name__=~"kube_pod_container_resource_requests|kube_pod_container_resource_limits|kube_node_status_capacity|kube_node_status_allocatable", kubernetes_node=~"${options.nodes}", resource="memory"}) by (__name__, component)`;
 
                 case "node-cpu-stats":
-                    return `sum(rate(node_cpu_seconds_total{node=~"${options.nodes}", mode=~"user|system"}[1m]))`;
+                    return `sum(rate(node_cpu_seconds_total{kubernetes_node=~"${options.nodes}", mode=~"user|system"}[1m]))`;
                 case "kube-cpu-stats":
-                    return `sum({__name__=~"kube_pod_container_resource_requests|kube_pod_container_resource_limits|kube_node_status_capacity|kube_node_status_allocatable", node=~"${options.nodes}", resource="cpu"}) by (__name__, component)`;
+                    return `sum({__name__=~"kube_pod_container_resource_requests|kube_pod_container_resource_limits|kube_node_status_capacity|kube_node_status_allocatable", kubernetes_node=~"${options.nodes}", resource="cpu"}) by (__name__, component)`;
             }
             break;
         case "pods":
