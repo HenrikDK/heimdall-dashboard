0896dd10a52098a2ae336a5104a0f7c7105fc51c
HenrikDK <commander.john.crichton@gmail.com>
2024-04-10 00:44:28 +0200

    work on unit handling

--- Heimdall.Dashboard.Ui/wwwroot/js/site.js
+++ Heimdall.Dashboard.Ui/wwwroot/js/site.js
@@ -132,18 +132,91 @@ function getMetricLastPoints(data, metric= '') {
     return parseFloat(result[0]);
 }
 
-function getMetricSeries(data, metric= '') {
-    let metrics = data
-    if (metric.length > 0){
-        metrics = metrics.filter(x => x.metric["__name__"] === metric)
-    }
-    let result = metrics.map(x => {
-        try {
-            return x.values;
-        } catch {
-            return undefined;
+function getDataSeries(options) {
+    if (options.length < 1) return [];
+    
+    let time = options[0].metrics.values.map(x => x[0]);
+    let first = options[0].metrics.values.map(x => parseFloat(x[1]));
+    let second = options.length > 1 ? options[1].metrics.values.map(x => parseFloat(x[1])) : [];
+    let firstMax = Math.max(first);
+    let secondMax = second.length > 0 ? Math.max(second) : 0;
+    let trueMax = firstMax > secondMax ? firstMax : secondMax;
+
+    let unit = first?.params?.unit === 'bytes' ? getUnitFromBytes(firstMax) : {suffix: "", magnitude: 1};
+
+    let data = time.map((t, index) => {
+        let short = luxon.DateTime.fromMillis(t * 1000).toFormat('HH:mm');
+
+        if (second.length < 1){
+            return [short, first[index]]
+        }
+
+        if (firstMax > secondMax){
+            return [short, second[index], first[index]];
+        } else{
+            return [short, first[index], second[index]];
         }
     });
 
-    return result[0];
+    options[0].params['max'] = firstMax;
+    options[0].data = data;
+    if (options.length > 1){
+        options[1].data = data;
+        options[1].params['max'] = secondMax;
+        options[1].params['index'] = secondMax > firstMax ? 2 : 1;
+        options[0].params['index'] = firstMax > secondMax ? 2 : 1;
+    }
+
+    return [data, trueMax, unit]
+}
+
+const base = 1024;
+const units = [
+  {suffix: "B", magnitude: 1},
+  {suffix: "KiB", magnitude: base ** 1},
+  {suffix: "MiB", magnitude: base ** 2},
+  {suffix: "GiB", magnitude: base ** 3},
+  {suffix: "TiB", magnitude: base ** 4},
+  {suffix: "PiB", magnitude: base ** 5},
+  {suffix: "EiB", magnitude: base ** 6},
+];
+
+const unitRegex = /(?<value>[0-9]+(\.[0-9]*)?)(?<suffix>(B|[KMGTPE]iB?))?/;
+
+function unitsToBytes(value) {
+  const match = value.match(unitRegex);
+
+  if (!match?.groups) {
+    return NaN;
+  }
+
+  const parsedValue = parseFloat(match.groups.value);
+
+  if (!match.groups?.suffix) {
+    return parsedValue;
+  }
+
+  const unit = units.filter(x => x.suffix === match.groups.suffix)[0];
+
+  return parseInt((parsedValue * unit.magnitude).toFixed(1));
+}
+
+function bytesToUnits(bytes, precision = 1){
+  let unit = getUnitFromBytes(bytes);
+
+  return `${(bytes / unit.magnitude).toFixed(precision)}${unit.suffix}`;
+}
+
+function getUnitFromBytes(bytes){
+    if (bytes <= 0 || isNaN(bytes) || !isFinite(bytes)) {
+        return "N/A";
+    }
+
+    const index = Math.floor(Math.log(bytes) / Math.log(base));
+
+    if (index < magnitudes.length - 1){
+        return magnitudes[index];
+    }
+    
+    return magnitudes[magnitudes.length -1];
 }
